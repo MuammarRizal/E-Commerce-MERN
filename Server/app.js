b35a97922d4c5d6ebaed2f8fcb4313e23e4dedc0
MuammarRizal <muammarrizal31@gmail.com>
2024-12-24 21:36:35 +0700

    setup database MONGODB

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import router from "./src/routes/authRouter.js";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 const PORT = 4000;
 
@@ -9,3 +11,13 @@ app.use("/api/v1/auth", router);
 
 // server
 app.listen(PORT, () => console.log(`Server Runinng on port : ${PORT}`));
+
+// Connection DB
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => {
+    console.log("Database Connect");
+  })
+  .catch(() => {
+    console.log("Database is not connection");
+  });
